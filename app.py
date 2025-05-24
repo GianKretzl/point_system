12db3049329fad0f62a2403ec969b58fa683568b
GianKretzl <gian.kretzl@outlook.com>
2025-05-24 15:12:31 -0300

    fix app.py

--- app.py
+++ app.py
@@ -5,10 +5,21 @@ from models import User
 
 app = Flask(__name__)
 app.register_blueprint(bp)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///point_system.db'
 db.init_app(app)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  
+        db.create_all()
+        # Criação automática do usuário admin
+        if not User.query.filter_by(username='GianKretzl').first():
+            user = User(
+                name='Gian Felipe Kretzl',
+                username='GianKretzl',
+                password='123456',  # Use hash em produção!
+                is_admin=True,
+                is_employee=False
+            )
+            db.session.add(user)
+            db.session.commit()
     app.run(debug=True)
