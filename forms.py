614f12c26f314cda7c0fe75e429f00ff37c4d94e
GianKretzl <gian.kretzl@outlook.com>
2025-05-25 16:16:46 -0300

    .

--- forms.py
+++ forms.py
@@ -2,7 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
+# Formulário de login do sistema
 class LoginForm(FlaskForm):
+    # Campo para o nome de usuário (obrigatório)
     username = StringField('Usuário', validators=[DataRequired()])
+    # Campo para a senha (obrigatório)
     password = PasswordField('Senha', validators=[DataRequired()])
+    # Botão de envio do formulário
     submit = SubmitField('Entrar')
